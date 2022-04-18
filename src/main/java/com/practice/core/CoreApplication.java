2440b390f83b29c0a699524578cbd0604b1c1cd0
sihyunpark <bags26578@gmail.com>
2022-04-18 19:27:56 +0900

    secodn messgage

--- src/main/java/com/practice/core/CoreApplication.java
+++ src/main/java/com/practice/core/CoreApplication.java
@@ -1,5 +1,9 @@
 package com.practice.core;
 
+import com.practice.core.member.Grade;
+import com.practice.core.member.Member;
+import com.practice.core.member.MemberService;
+import com.practice.core.member.MemberServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoreApplication {
 
 	public static void main(String[] args) {
+		MemberService memberService = new MemberServiceImpl();
+		Member member = new Member(1L, "memberA", Grade.VIP);
+		memberService.join(member);
+
+		Member findMember = memberService.findMember(1L);
+		System.out.println("new member = " + member.getName());
+		System.out.println("findMember = " + findMember.getName());
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
