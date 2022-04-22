3ba7d02c350fab0618161d3f704fca278b1f4af9
sihyunpark <bags26578@gmail.com>
2022-04-22 20:57:56 +0900

    4/22 commit (~48page)

--- src/main/java/com/practice/core/AppConfig.java
+++ src/main/java/com/practice/core/AppConfig.java
@@ -8,13 +8,18 @@ import com.practice.core.member.MemberServiceImpl;
 import com.practice.core.member.MemoryMemberRepository;
 import com.practice.core.order.OrderService;
 import com.practice.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 new MemoryMemberRepository(),
@@ -22,10 +27,12 @@ public class AppConfig {
         );
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
--- src/main/java/com/practice/core/MemberApp.java
+++ src/main/java/com/practice/core/MemberApp.java
@@ -4,14 +4,20 @@ import com.practice.core.member.Grade;
 import com.practice.core.member.Member;
 import com.practice.core.member.MemberService;
 import com.practice.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
 
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService =
+                applicationContext.getBean("memberService", MemberService.class);
 
         long memberId = 1L;
 
